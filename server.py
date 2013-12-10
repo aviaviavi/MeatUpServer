d96781b0f63df075403759ad9d729216e3ea9682
aviaviavi <avipress@gmail.com>
2013-12-09 20:35:02 -0800

    work in progress, sync data

--- server.py
+++ server.py
@@ -1,14 +1,32 @@
 import os
 from flask import Flask
-from flask.ext.sqlalchemy import SQLAlchemy
+import pdb
+#from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
-db = SQLAlchemy(app)
+#app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+#db = SQLAlchemy(app)
 
 @app.route('/')
 def hello():
     return 'Hello World!'
 
+@app.route('/sync_data', methods="POST")
+def sync():
+    print "sync called"
+    print request.method
+    pdb.set_trace()
+    if request.method == "POST":
+        print "in if"
+        print request
+    elif request.method == "GET":
+        print "get"
+    else:
+        print "none"
+    print "past"
+    return "Data Recieved"
+    
+
 if __name__ == '__main__':
-    app.run()
+    print "here"
+    app.run(host='0.0.0.0')
