50375f7363167fb425d4489cb66cb6abe3769e94
Danny Weinberg <FuegoFro@gmail.com>
2013-12-09 22:04:16 -0800

    Basic updates route

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
+import time
 import os
-from flask import Flask
+from flask import Flask, request, json
 import pdb
 #from flask.ext.sqlalchemy import SQLAlchemy
 
@@ -7,15 +8,18 @@ app = Flask(__name__)
 #app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 #db = SQLAlchemy(app)
 
+
+lunch_requests = {}  # Maps phone numbers to lunch requests
+
+
 @app.route('/')
 def hello():
     return 'Hello World!'
 
-@app.route('/sync_data', methods="POST")
+@app.route('/sync_data', methods=["GET"])
 def sync():
     print "sync called"
     print request.method
-    pdb.set_trace()
     if request.method == "POST":
         print "in if"
         print request
@@ -27,6 +31,21 @@ def sync():
     return "Data Recieved"
     
 
+@app.route('/update', methods=["GET"])
+def update():
+    identifier = request.args.get('identifier')
+    data = lunch_requests.pop(identifier, {})
+    return json.dumps(data)
+
+
+@app.route('/test_add_data', methods=["GET"])
+def add_data():
+    lunch_requests["9876543210"] = {
+        'lunches': [{'time': time.time(), 'people': ['Danny', 'Colorado', 'Avi', 'Lexi'], 'place': 'Sliver Pizzeria'}],
+        'friend_invites': [],
+    }
+    return "ok"
+
+
 if __name__ == '__main__':
-    print "here"
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', debug=True)
